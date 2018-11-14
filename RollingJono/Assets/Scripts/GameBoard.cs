7606cdae9016f07fabe6be5f87cf24410bccb8cc
Arthur Uzulin <fuj1nman@hotmail.com>
2018-11-14 16:02:29 +1100

    Added board and ball

---
+++ RollingJono/Assets/Scripts/GameBoard.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class GameBoard : MonoBehaviour
+{
+    public float sensitivity = 25F;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        JoyControl();
+    }
+
+    private void JoyControl()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        float yaw = Input.GetAxis("Yaw");
+
+        rb.MoveRotation(transform.rotation * Quaternion.Euler(new Vector3(vertical, yaw, -horizontal) * sensitivity * Time.deltaTime));
+    }
+}
