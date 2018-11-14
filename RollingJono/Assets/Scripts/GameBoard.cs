75f310886788cc6f74060691ff55771f99d0541d
Arthur Uzulin <fuj1nman@hotmail.com>
2018-11-14 20:02:40 +1100

    Made board rotate based on the camera and fixed ball clipping

--- RollingJono/Assets/Scripts/GameBoard.cs
+++ RollingJono/Assets/Scripts/GameBoard.cs
@@ -3,6 +3,7 @@
 public class GameBoard : MonoBehaviour
 {
     public float sensitivity = 25F;
+    public Camera pivotCamera;
 
     private Rigidbody rb;
 
@@ -18,10 +19,29 @@ public class GameBoard : MonoBehaviour
 
     private void JoyControl()
     {
-        float vertical = Input.GetAxis("Vertical");
-        float horizontal = Input.GetAxis("Horizontal");
+        float pitch = Input.GetAxis("Horizontal");
         float yaw = Input.GetAxis("Yaw");
+        float roll = Input.GetAxis("Vertical");
 
-        rb.MoveRotation(transform.rotation * Quaternion.Euler(new Vector3(vertical, yaw, -horizontal) * sensitivity * Time.deltaTime));
+        rb.MoveRotation(CameraRelativeRotation(pitch, yaw, roll));
+    }
+
+    private Quaternion CameraRelativeRotation(float pitch, float yaw, float roll)
+    {
+        if (!pivotCamera)
+            return transform.rotation * Quaternion.Euler(new Vector3(roll, yaw, -pitch) * sensitivity * Time.deltaTime);
+
+        Vector3 relativePitch = pivotCamera.transform.TransformDirection(Vector3.down);
+        Vector3 relativeYaw = pivotCamera.transform.TransformDirection(Vector3.back);
+        Vector3 relativeRoll = pivotCamera.transform.TransformDirection(Vector3.right);
+
+        Vector3 objectRelativePitch = transform.InverseTransformDirection(relativePitch);
+        Vector3 objectRelaviveYaw = transform.InverseTransformDirection(relativeYaw);
+        Vector3 objectRelaviveRoll = transform.InverseTransformDirection(relativeRoll);
+
+        return transform.rotation
+                * Quaternion.AngleAxis(pitch * sensitivity * Time.deltaTime, objectRelativePitch)
+                * Quaternion.AngleAxis(yaw * sensitivity * Time.deltaTime, objectRelaviveYaw)
+                * Quaternion.AngleAxis(roll * sensitivity * Time.deltaTime, objectRelaviveRoll);
     }
 }
