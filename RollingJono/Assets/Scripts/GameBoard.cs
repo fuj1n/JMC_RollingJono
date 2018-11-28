343c7afdb919b420160587845fd1c2c0e7c1a38e
Arthur Uzulin <fuj1nman@hotmail.com>
2018-11-28 14:11:39 +1100

    Fix relative rotations

--- RollingJono/Assets/Scripts/GameBoard.cs
+++ RollingJono/Assets/Scripts/GameBoard.cs
@@ -3,7 +3,7 @@
 public class GameBoard : MonoBehaviour
 {
     public float sensitivity = 25F;
-    public Camera pivotCamera;
+    public Transform sourceVector;
 
     private Rigidbody rb;
 
@@ -20,28 +20,28 @@ public class GameBoard : MonoBehaviour
 
     private void JoyControl()
     {
-        float pitch = Input.GetAxis("Horizontal");
+        float pitch = Input.GetAxis("Vertical");
         float yaw = Input.GetAxis("Yaw");
-        float roll = Input.GetAxis("Vertical");
+        float roll = Input.GetAxis("Horizontal");
 
         rb.MoveRotation(transform.rotation * CameraRelativeRotation(pitch, yaw, roll));
     }
 
     private Quaternion CameraRelativeRotation(float pitch, float yaw, float roll)
     {
-        if (!pivotCamera)
-            return Quaternion.Euler(new Vector3(roll, yaw, -pitch) * sensitivity * Time.deltaTime);
+        if (!sourceVector)
+            return Quaternion.Euler(new Vector3(pitch, yaw, -roll) * sensitivity * Time.deltaTime);
 
-        Vector3 relativePitch = pivotCamera.transform.TransformDirection(Vector3.down);
-        Vector3 relativeYaw = pivotCamera.transform.TransformDirection(Vector3.back);
-        Vector3 relativeRoll = pivotCamera.transform.TransformDirection(Vector3.right);
+        Vector3 relativePitch = sourceVector.TransformDirection(Vector3.right);
+        Vector3 relativeYaw = sourceVector.TransformDirection(Vector3.down);
+        Vector3 relativeRoll = sourceVector.TransformDirection(Vector3.back);
 
-        Vector3 objectRelativePitch = transform.InverseTransformDirection(relativePitch);
-        Vector3 objectRelaviveYaw = transform.InverseTransformDirection(relativeYaw);
+        Vector3 objectRelavivePitch = transform.InverseTransformDirection(relativePitch);
+        Vector3 objectRelativeYaw = transform.InverseTransformDirection(relativeYaw);
         Vector3 objectRelaviveRoll = transform.InverseTransformDirection(relativeRoll);
 
-        return Quaternion.AngleAxis(pitch * sensitivity * Time.deltaTime, objectRelativePitch)
-             * Quaternion.AngleAxis(yaw * sensitivity * Time.deltaTime, objectRelaviveYaw)
+        return Quaternion.AngleAxis(pitch * sensitivity * Time.deltaTime, objectRelavivePitch)
+             * Quaternion.AngleAxis(-yaw * sensitivity * Time.deltaTime, objectRelativeYaw)
              * Quaternion.AngleAxis(roll * sensitivity * Time.deltaTime, objectRelaviveRoll);
     }
 
